b3030886a85d247f4a5fd1ca7202e8dbadf233ac
vik <39262485+ViktorKornilov@users.noreply.github.com>
2024-02-18 17:33:16 +0200

    Weapon upgrade

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -4,11 +4,61 @@ public class Weapon : MonoBehaviour
 {
     public GameObject bulletPrefab;
 
+    public int ammo;
+    public int maxAmmo = 10;
+    public bool isReloading;
+    public bool isAutomatic;
+    public float fireInterval = 0.1f;
+    public float fireCooldown;
+    public float reloadTime = 2;
+
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Mouse0))
+        // manual shooting
+        if (!isAutomatic && Input.GetKeyDown(KeyCode.Mouse0))
         {
-            Instantiate(bulletPrefab, transform.position, transform.rotation);
+            Shoot();
         }
+        // automatic shooting
+        if (isAutomatic && Input.GetKey(KeyCode.Mouse0))
+        {
+            Shoot();
+        }
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            Reload();
+        }
+        fireCooldown -= Time.deltaTime;
+    }
+
+    void Shoot()
+    {
+        if(isReloading) return;
+        if (ammo <= 0)
+        {
+            Reload();
+            return;
+        }
+        if(fireCooldown > 0) return;
+
+        ammo--;
+        fireCooldown = fireInterval;
+        Instantiate(bulletPrefab, transform.position, transform.rotation);
+    }
+
+    async void Reload()
+    {
+        if (ammo == maxAmmo) return;
+        if(isReloading) return;
+
+        isReloading = true;
+
+        print ("Reloading...");
+        await new WaitForSeconds(reloadTime);
+        print("Reloaded!");
+
+        isReloading = false;
+        ammo = maxAmmo;
     }
 }
