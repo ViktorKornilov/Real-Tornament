f3aa302635eab157aa8eb9f1f67c3efccf36881c
vik <39262485+ViktorKornilov@users.noreply.github.com>
2024-02-18 16:53:45 +0200

    Health, Rocket Damage

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    public int hp;
+    public int maxHp = 100;
+
+    void Start()
+    {
+        if(hp == 0)hp = maxHp;
+    }
+
+    public void Damage(int damage)
+    {
+        hp -= damage;
+        if(hp <= 0)
+        {
+            Die();
+        }
+    }
+
+    public void Die()
+    {
+        Destroy(gameObject);
+    }
+}
--- Assets/Scripts/Rocket.cs
+++ Assets/Scripts/Rocket.cs
@@ -4,8 +4,24 @@ public class Rocket : MonoBehaviour
 {
     public float speed = 20;
 
+    void Start()
+    {
+        Destroy(gameObject, 3);
+    }
+
     void Update()
     {
         transform.position += transform.forward * speed * Time.deltaTime;
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        var health = other.gameObject.GetComponent<Health>();
+        if(health != null)
+        {
+            health.Damage(10);
+        }
+
+        Destroy(gameObject);
     }
 }
