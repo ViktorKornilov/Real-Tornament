be0b60671916c2ddc65dc875ecf71f59dbf23838
vik <39262485+ViktorKornilov@users.noreply.github.com>
2024-02-25 10:32:21 +0200

    Health With Events

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -1,11 +1,15 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class Health : MonoBehaviour
 {
     public int health;
     public int maxHealth = 100;
+    public bool shouldDestroy = true;
+    public UnityEvent onDie;
+    public UnityEvent<int> onDamage;
+    public GameObject damageEffect;
+    public GameObject deathEffect;
 
     void Start()
     {
@@ -16,14 +20,20 @@ public class Health : MonoBehaviour
     public void Damage(int damage)
     {
         health -= damage;
+        onDamage.Invoke(health);
         if(health <= 0)
         {
             Die();
         }
+
+        if(health < 0)health = 0;
+        if(damageEffect != null)Instantiate(damageEffect, transform.position, Quaternion.identity);
     }
 
     public void Die()
     {
-        Destroy(gameObject);
+        onDie.Invoke();
+        if(shouldDestroy)Destroy(gameObject);
+        if(deathEffect != null)Instantiate(deathEffect, transform.position, Quaternion.identity);
     }
 }
