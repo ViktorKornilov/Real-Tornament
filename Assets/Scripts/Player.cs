7929cf5c975fae2bc92561a04ba527fe57977516
vik <39262485+ViktorKornilov@users.noreply.github.com>
2024-02-25 15:16:41 +0200

    Respawn

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,4 +1,5 @@
 using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Player : MonoBehaviour
@@ -13,8 +14,11 @@ public class Player : MonoBehaviour
 
     void Start()
     {
+        UpdateUI();
+
         weapon.onShoot.AddListener(UpdateUI);
         health.onDamage.AddListener(UpdateUI);
+        health.onDie.AddListener(Respawn);
     }
 
     void UpdateUI()
@@ -30,5 +34,12 @@ public class Player : MonoBehaviour
         {
             health.Damage(10);
         }
+    }
+
+    void Respawn()
+    {
+        health.health = health.maxHealth;
+        transform.position = Vector3.zero;
+        UpdateUI();
     }
 }
